ed85c453300e87138865463fb416c0884573d953
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-06-06 23:02:38 +0200

    Update 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    last_d = number % 10
+    last_digit = number % 10
+    print(last_digit)
     
-    print(last_d)
-    return last_d
+    return last_digit
