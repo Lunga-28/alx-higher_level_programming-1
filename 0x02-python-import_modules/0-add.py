390516a8e565bf71dbaf447f6e421593fdaa5089
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-06-09 00:48:32 +0200

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-from add_0 import add
-
-if __name__ == "__main__":
 a = 1
 b = 2
+add_0 = __import__('add_0')
+add_result = add_0.add(a, b)
+print("{} + {} = {}".format(a, b, add_result))
 
- print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
