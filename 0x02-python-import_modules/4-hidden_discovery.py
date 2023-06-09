ffa40e0c359f531ac5370032b93334f9d3d4ebb0
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-06-09 21:17:46 +0200

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for word in dir(hidden_4):
-        if word[:2] != "__":
-            print("{:s}".format(word))
+    for code in dir(hidden_4):
+        if code[:2] != "__":
+            
+            print("{:s}".format(code))
