4541c818ce4781f9ec7b91cefcc3af13a2d007f6
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-06-09 21:16:00 +0200

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python3
-
-import dis
-
 if __name__ == "__main__":
-    with open("hidden_4.pyc", "rb") as file:
-        code = file.read()
-
-    bytecode = dis.Bytecode(code)
-
-    names = set()
-    for instr in bytecode:
-        if instr.opname == "LOAD_CONST":
-            const = instr.argval
-            if isinstance(const, str) and not const.startswith("__"):
-                names.add(const)
-
-    for name in sorted(names):
-        print(name)
-
+    import hidden_4
+    for word in dir(hidden_4):
+        if word[:2] != "__":
+            print("{:s}".format(word))
