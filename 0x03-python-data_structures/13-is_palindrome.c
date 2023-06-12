055633e4d9db5d4d95604c21d6fff082de2e6daf
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-06-13 01:01:04 +0200

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,69 +1,69 @@
 #include "lists.h"
 
 /**
- * reverse_listint - ...
- * @head: ...
+ * reverse_listint - Reverse a linked list
+ * @head: Pointer to the head of the list
  *
- * Return: ...
+ * Return: Pointer to the new head of the reversed list
  */
 void reverse_listint(listint_t **head)
 {
-	listint_t *prevs = NULL;
-	listint_t *curr = *head;
-	listint_t *nex = NULL;
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
 
-	while (curr)
+	while (current != NULL)
 	{
-		nex = curr->nex;
-		curr->nex = prevs;
-		prevs = curr;
-		curr = nex;
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
 	}
-	*head = prevs;
+	*head = prev;
 }
 
 /**
- * is_palindrome - ...
- * @head: ...
+ * is_palindrome - Check if a linked list is a palindrome
+ * @head: Pointer to the head of the list
  *
- * Return: ...
+ * Return: 1 if the list is a palindrome, 0 otherwise
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slo = *head, *fast = *head, *temp = *head, *dupli = NULL;
+	listint_t *slow = *head, *fast = *head, *temp = *head, *duplicate = NULL;
 
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
+
 	while (1)
 	{
 		fast = fast->next->next;
 		if (!fast)
 		{
-			dupli = slo->next;
+			duplicate = slow->next;
 			break;
 		}
 		if (!fast->next)
 		{
-			dupli = slo->next->next;
+			duplicate = slow->next->next;
 			break;
 		}
-		slo = slow->next;
+		slow = slow->next;
 	}
 
-	reverse_listint(&dupli);
-
-	while (dupli && temp)
+reverse_listint(&duplicate);
+while (duplicate && temp)
 	{
-		if (temp->n == dupli->n)
+		if (temp->n == duplicate->n)
 		{
-			dupli = dupli->next;
+			duplicate = duplicate->next;
 			temp = temp->next;
 		}
 		else
 			return (0);
 	}
 
-	if (!dupli)
+	if (!duplicate)
 		return (1);
 
 	return (0);
