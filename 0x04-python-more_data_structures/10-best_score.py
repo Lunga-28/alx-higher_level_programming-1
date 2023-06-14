1e314a262c01d7ea95b56b719bee5ae5716f4287
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-06-15 01:34:27 +0200

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max_score = None
+    
+    max_scor = None
+    
     max_key = None
 
     for key, value in a_dictionary.items():
-        if max_score is None or value > max_score:
-            max_score = value
+        
+        if max_scor is None or value > max_scor:
+            
+            max_scor = value
             max_key = key
 
    
