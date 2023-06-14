65896359e5705c7cc87cdb4d15c96af53f6b6f3e
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-06-14 20:33:21 +0200

    Create 3-common_elements.py

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    common_set = set()
+
+    # Iterate over each element in the first set
+    for elem in set_1:
+        if elem in set_2:
+
+            common_set.add(elem)
+
+    return common_set
