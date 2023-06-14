e179246fb8ef42f9e3ba9fdc361d8f2b5ba00412
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-06-15 01:36:44 +0200

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    
     if len(my_list) == 0:
         return 0
     
-    total_score = 0
+    total_scor = 0
+    
     total_weight = 0
 
     for score, weight in my_list:
-        total_score += score * weight
+        
+        total_scor += score * weight
+        
         total_weight += weight
     
-    weighted_average = total_score / total_weight
+    weighted_average = total_scor / total_weight
+    
     return weighted_average
