191f8e7d9a8908741c8d5b92397565a95775b9f1
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-06-14 19:30:07 +0200

    Create 1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    resul = []
+    for item in my_list:
+        if item == search:
+            resul.append(replace)
+        else:
+            resul.append(item)
+t
+    return resul
