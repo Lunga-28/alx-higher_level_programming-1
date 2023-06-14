1b6ffde74ebd12374028d52f34fcf1a560c93274
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-06-15 00:00:11 +0200

    Update 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
 def print_sorted_dictionary(a_dictionary):
-    for srt in sorted(a_dictionary.keys()):
-print("{}: {}".format(srt, a_dictionary[srt]))
+
+    for value in sorted(a_dictionary.keys()):
+
+        print("{}: {}".format(value, a_dictionary[value]))
