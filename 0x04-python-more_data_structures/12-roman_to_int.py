fecd390b7d973a2c28322f639729c4b97ed2f32a
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-06-15 01:40:41 +0200

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
     
@@ -9,14 +10,18 @@ def roman_to_int(roman_string):
     }
     
     result = 0
+    
     prev_value = 0
     
     for char in reversed(roman_string):
         value = roman_value.get(char, 0)
+        
         if value >= prev_value:
+            
             result += value
         else:
             result -= value
+            
         prev_value = value
     
     return result
