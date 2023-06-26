ebcce97da31775a180ba2fce6de9814f7ada8a9b
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-06-26 20:24:44 +0200

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        return None
+    except Exception as e:
+        print("An error occurred:", str(e))
+        return None
+    finally:
+        print("Inside result: {}".format(result) if 'result' in locals() else "Inside result: None")
+        return result if 'result' in locals() else None
 
