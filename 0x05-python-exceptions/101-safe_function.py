89f801da72106fcfe7346368d59e74dee2ff5646
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-06-27 00:15:13 +0200

    Update 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python3
+
+import sys
+
+def safe_function(fct, *args):
+    try:
+        resu = fct(*args)
+        return resu
+    except Exception as e:
+        print("Exception:", str(e), file=sys.stderr)
+        return None
 
