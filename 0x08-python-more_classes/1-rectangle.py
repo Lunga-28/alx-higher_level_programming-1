0f1d3119f2ef4def593ed51b0eb536db420afa51
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-07-03 23:51:51 +0200

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
+"""
+This module defines a class - Rectangle
+"""
+
 class Rectangle:
+    """
+    class has two attributes
+    width
+    height
+    """
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """
+        function to return width if setter checks have passed
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -18,10 +32,14 @@ class Rectangle:
 
     @property
     def height(self):
+        """
+        return height if setter checks have passed
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
