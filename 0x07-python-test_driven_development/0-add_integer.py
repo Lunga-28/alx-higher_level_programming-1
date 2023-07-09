a153703ec8aea6faf07d77020a004b4496b30050
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-07-09 21:32:32 +0200

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,44 +1,24 @@
 #!/usr/bin/python3
 
-
 """
 
-This module adds two integers and returns sum of two numbers a & b.
+adds two integers and returns sum of two numbers a & b.
 
-Only accepts integers and floats else TypeError is raised
+accepts integers and floats else TypeError is raised
 
-This module will convert float to integer
+convert float to integer
 
 """
 
 def add_integer(a, b=98):
-    """
-    Adds two integers
 
-    Args:
-        a (int or float): First number
-        b (int or float): Second number (default is 98)
 
-    Returns:
-        int: The addition of a and b
+    if isinstance(a, (int, float)) is False:
 
-    Raises:
-        TypeError: If a or b is not an integer or float
+        raise TypeError('a must be an integer')
 
-    Examples:
-        >>> add_integer(1, 2)
-        3
-        >>> add_integer(100, -2)
-        98
-        >>> add_integer(2)
-        100
-        >>> add_integer(100.3, -2)
-        98
+    elif isinstance(b, (int, float)) is False:
 
-    """
-if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or float")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer or float")
+        raise TypeError('b must be an integer')
 
-    return int(a) + int(b)
+    return(int(a) + int(b))
