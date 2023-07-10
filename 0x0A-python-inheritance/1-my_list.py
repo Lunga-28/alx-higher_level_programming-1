5092619c718f88d423b7e9540cce4df2f1c39e98
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-07-10 21:53:52 +0200

    Create 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+class that inherits from list
+"""
+
+class MyList(list):
+    """
+    class that inherits
+    """
+    def print_sorted(self):
+        print(sorted(self))
