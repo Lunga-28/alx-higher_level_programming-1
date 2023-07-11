2c5eb994d5667167c2b02aa51148f2a2ad1fb767
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-07-11 20:38:20 +0200

    Create 5-save_to_json_file.py

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+JSON to text
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """writes an object to a text file"""
+
+  
+    with open(filename, mode="w") as f:
+        json.dump(my_obj, f)
