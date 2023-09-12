3d30bcfcd17d1a038be83b2fbdf7caf39cca99be
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-09-12 21:09:14 +0200

    Create 4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let j = 0; j < this.height; j++) {
+      console.log(Array(this.width + 1).join('X'));
+    }
+  }
+
+  rotate () {
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
