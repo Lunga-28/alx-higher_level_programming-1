fb9c72f9c37d02d0c4d13e15c8f722f35b13915b
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-09-17 15:54:37 +0200

    Create 4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+list all cities
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+    JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+    lst = cur.fetchall()
+    for r in lst:
+        print(r)
+    cur.close()
+    db.close()
