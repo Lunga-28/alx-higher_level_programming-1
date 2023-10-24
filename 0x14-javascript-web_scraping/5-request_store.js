234e7ac340810970ad78f55695b43b961fdc0916
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-10-24 19:57:35 +0200

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 // get the contents of a webpage and stores them in a file
-let request = require('request');
-let fs = require('fs');
-request(process.argv[2], function (error, response, body) {
-  if (error) throw error;
-  fs.writeFile(process.argv[3], body, function (err) {
-    if (err) throw err;
-  });
-});
+
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, 'utf8').pipe(fs.createWriteStream(file));
