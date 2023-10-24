14486c635653753bf36cf55b16516ae24869dfc3
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-10-24 19:58:39 +0200

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,34 +1,25 @@
 #!/usr/bin/node
 // computes number of tasks completed by user id
-let request = require('request');
-let idList = [];
-let dict = {};
-let options = {
-  url: process.argv[2],
-  headers: {
-    'format': 'json'
-  }
-};
 
-function countEle (list, searchElement) {
-  let count = 0;
-  for (let element in list) {
-    if (searchElement === list[element]) {
-      count++;
-    }
-  }
-  return count;
-}
-function callback (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    let res = JSON.parse(body);
-    for (let item in res) {
-      if (res[item]['completed'] === true) {
-        idList.push(res[item]['userId']);
-        dict[res[item]['userId']] = countEle(idList, res[item]['userId']);
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const todos = JSON.parse(body);
+    const users = {};
+
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].completed) {
+        if (users[todos[i].userId]) {
+          users[todos[i].userId] += 1;
+        } else {
+          users[todos[i].userId] = 1;
+        }
       }
     }
+    console.log(users);
   }
-  console.log(dict);
-}
-request(options, callback);
+});
