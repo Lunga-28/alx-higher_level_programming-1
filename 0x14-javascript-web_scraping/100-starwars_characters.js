9a7fa8cd7e0399358d0d001246f2442367359361
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-10-24 19:59:17 +0200

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,27 +1,18 @@
 #!/usr/bin/node
 // prints characters of a Star Wars movie
-let request = require('request');
-let options = {
-  url: 'http://swapi.co/api/films/' + process.argv[2],
-  headers: {
-    'format': 'json'
-  }
-};
-function callback (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    let res = JSON.parse(body);
-    for (let item in res['characters']) {
-      request(res['characters'][item], function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-          let characters = JSON.parse(body);
-          for (let i in characters) {
-            if (i === 'name') {
-              console.log(characters[i]);
-            }
-          }
+
+const request = require('request');
+const film = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${film}`;
+request(url, (err, res, body) => {
+  if (!err) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (err, res, body) {
+        if (!err) {
+          console.log(JSON.parse(body).name);
         }
       });
-    }
+    });
   }
-}
-request(options, callback);
+});
