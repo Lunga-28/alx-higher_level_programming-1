993e0135c5160f6b6aff77544b437afa9584b532
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-10-24 19:53:57 +0200

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
 // reads then prints the content of file
-let fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (err) { console.log(err); } else { process.stdout.write(data); }
+
+const filePath = process.argv[2];
+const fs = require('fs');
+
+fs.readFile(filePath, 'utf8', function (error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
 });
