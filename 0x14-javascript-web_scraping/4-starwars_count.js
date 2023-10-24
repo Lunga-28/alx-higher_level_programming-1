31d0b72c7afbc857c88a8a97c4d736df3fff3d2f
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-10-24 19:44:25 +0200

    Create 4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// prints number of movies where "Wedge Antilles" is present
+let request = require('request');
+let searchEle = 'https://swapi.co/api/people/18/';
+let count = 0;
+let options = {
+  url: process.argv[2],
+  headers: {
+    'format': 'json'
+  }
+};
+function callback (error, response, body) {
+  if (error) { console.log(error); } else {
+    let res = (JSON.parse(body));
+    for (let item in res['results']) {
+      if (res['results'][item]['characters'].indexOf(searchEle) !== -1) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+}
+request(options, callback);
